9b034b65391a4fe438625e730a296cd27073a41e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-05 11:57:46 +0200

    "Added Header component, updated App.js to use it, and added global styles to App.scss and _variable.scss"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Umut</h1>
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
-import "./Header";
+import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h1>Product List</h1>
+      <ul>
+        <li>All</li>
+        <li>Electronics</li>
+        <li>Jewelery</li>
+        <li>Men's Clothing</li>
+        <li>Women's Clothing</li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
